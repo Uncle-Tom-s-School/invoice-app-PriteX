6f4c3ac4bad87ef6391a1431ffe19eec1e96957e
PriteX <peterpalotai06@gmail.com>
2025-12-08 13:35:08 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-const App = () => {
+import React from "react";
+import Invoice from "./components/Invoice";
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="app">
+      <Invoice />
+    </div>
+  );
 }
 
-export default App
+export default App;
